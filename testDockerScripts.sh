74cb06b8a275618c89cb97fea7f54883f6328bc9
Damian Szymański <damian@assembla.com>
2019-05-06 16:48:13 +0200

    wip

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -13,7 +13,7 @@ done
 for i in $(seq 20); do
     echo "Running with docker number ${i}"
     docker run --rm -v $(pwd):/data/ openjdk:8u212 \
-        apt update -qq ; apt -qq install -y strace ; strace -s 1024 curl --connect-timeout 1 -ivvv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
+        curl --connect-timeout 1 -ivvv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
     echo
     echo "Sleeping 10 seconds..."
     sleep 10
