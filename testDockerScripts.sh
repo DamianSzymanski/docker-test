a8f890bb2a494c1360277df098e3f10a5732c532
Damian Szymański <damian@assembla.com>
2019-05-06 21:31:04 +0200

    wip

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -12,7 +12,7 @@ done
 
 for i in $(seq 20); do
     echo "Running with docker number ${i}"
-    docker run --network=host --rm -v $(pwd):/data/ openjdk:8u212 \
+    docker run --rm -v $(pwd):/data/ openjdk:8u212 \
         curl --connect-timeout 1 -ivvv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
     echo
     echo "Sleeping 10 seconds..."
