00f1c7a725ba69b043d2828ca612882812979bda
Damian Szymański <damian@assembla.com>
2019-05-07 17:23:49 +0200

    wip

--- testDockerScripts.sh
+++ testDockerScripts.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 env
 
-for i in $(seq 1); do
+for i in $(seq 20); do
     echo "Running without docker number ${i}"
     curl -iv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
     echo
@@ -12,10 +12,10 @@ for i in $(seq 1); do
     sleep 10
 done
 
-for i in $(seq 1); do
+for i in $(seq 20); do
     echo "Running with docker number ${i}"
     docker run --rm -v $(pwd):/data/ openjdk:8u212 \
-        curl --connect-timeout 1 -ivvv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
+        curl --connect-timeout 1 -iv https://repo1.maven.org/maven2/org/broadinstitute/gatk/4.1.2.0/gatk-4.1.2.0.pom.md5
     echo
     echo "Sleeping 10 seconds..."
     sleep 10
